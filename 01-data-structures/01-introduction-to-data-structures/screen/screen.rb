a8e366996ae5bbe100b502fdb54076d609bb464c
compostbrain <joelcahalan@gmail.com>
2017-11-26 19:53:43 -0500

    immplemented screen class to pass all tests

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,41 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    @width = width
+    @height = height
+    @matrix = []
+
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    if inbounds(x, y)
+      pixel.x = x
+      pixel.y = y
+      @matrix.push(pixel)
+    end
   end
 
   def at(x, y)
+    if inbounds(x, y)
+      @matrix.each do |pixel|
+        if pixel.x == x && pixel.y == y
+          return pixel
+        end
+      end
+    else
+      nil
+    end
   end
 
   private
 
   def inbounds(x, y)
+      if x.between?(0, width) && y.between?(0, height)
+        true
+      else
+        false
+      end
   end
 
 end
