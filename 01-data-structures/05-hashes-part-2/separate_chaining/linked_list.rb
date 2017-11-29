0fa9c0c8d884d875b198e4590874538d6a00b87c
compostbrain <joelcahalan@gmail.com>
2017-11-29 14:01:47 -0500

    Implemented open addressing and seperate chaining

--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -6,25 +6,76 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    if self.head == nil
+      self.head = node
+    end
+    if self.tail
+      self.tail.next = node
+    end
+    node.next = nil
+    self.tail = node
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    current_tail = self.tail
+    current_head = self.head
+    if current_head != current_tail
+      current = self.head
+      until current.next == current_tail
+        current = current.next
+      end
+      add_to_tail(current)
+    else
+      self.tail = nil
+      self.head = nil
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    current_head = self.head
+    current_tail = self.tail
+    if current_head == current_tail
+      puts "The linked list has one member: #{current_head.data}"
+    else
+      current = current_head
+      string = ""
+      until current == current_tail
+        string += "#{current.data}\n"
+        current = current.next
+      end
+      string += "#{current.data}\n"
+      puts string
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if self.head == node
+      remove_front
+    elsif self.tail == node
+      remove_tail
+    else
+      current = self.head
+      while current.next != node
+        current = current.next
+      end
+      current.next = node.next
+      node.next = nil
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    node.next = self.head
+    self.head = node
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    temp = self.head
+    self.head = self.head.next
+    return temp
   end
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -1,33 +1,152 @@
 require_relative 'linked_list'
-
+require "pry"
 class SeparateChaining
   attr_reader :max_load_factor
 
   def initialize(size)
     @max_load_factor = 0.7
+    @items = Array.new(size)
   end
 
   def []=(key, value)
+    index = index(key, size)
+    node = Node.new(key, value)
+    # case 1: index is empty. Create a new LinkedList with node and
+    # add it to array at index
+    if @items[index] == nil
+      linked_list = LinkedList.new
+      linked_list.add_to_tail(node)
+      @items[index] = linked_list
+    # case 2: collision occurs and node is added to tail of existing
+    # linked list
+    else
+      linked_list = @items[index]
+      linked_list.add_to_tail(node)
+    end
   end
 
+  # iterate through the array to find the node with key == key
   def [](key)
+    (0...size).each do |index|
+      linked_list = @items[index]
+      if linked_list != nil
+        node = linked_list.head
+        until node.next == nil || node.key == key
+          node = node.next
+        end
+        return node.value if node.key == key
+      end
+    end
+    nil
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Calculate the current load factor
+  # number of values / number of buckets
   def load_factor
+    total_nodes = 0
+
+    (0...size).each do |index|
+      linked_list = @items[index]
+      if linked_list != nil
+        node = linked_list.head
+        if node.next == nil
+          total_nodes += 1
+        else
+          total_nodes += 1
+          until node.next == nil
+            node = node.next
+            total_nodes += 1
+          end
+        end
+      end
+    end
+
+    load_factor_calculation = total_nodes.to_f / size
+
+    if load_factor_calculation > @max_load_factor
+      resize
+      load_factor
+    else
+      load_factor_calculation
+    end
   end
 
   # Simple method to return the number of items in the hash
+  # need to check values per index
   def size
+   size = @items.length
   end
 
   # Resize the hash
   def resize
+    temp_hash = @items
+    @items = Array.new(temp_hash.length * 2)
+    temp_hash.each do |item|
+      if item != nil
+        new_hash_index = index(item.head.key, size)
+        @items[new_hash_index] = item
+      end
+    end
+  end
+
+  # Create a function capable of printing the state of your HashClass to the console.
+  # This function must present the location and value of each entry in a readily understood way.
+  # This function must also print the hash’s Load Factor.
+  # Use this function to verify the behavior of each HashClass after a series of insertions.
+
+  def print
+    total_nodes = 0
+    (0...size).each do |index|
+      linked_list = @items[index]
+      if linked_list != nil
+        node = linked_list.head
+
+        if node.next == nil
+          puts "Index #{index} contains:\nKey: #{node.key}\nValue: #{node.value}\n\n"
+          total_nodes += 1
+        else
+          puts "Index #{index} contains:\n"
+          total_nodes += 1
+          until node.next == nil
+            puts "Key: #{node.key}\n Value: #{node.value}\n"
+            node = node.next
+            total_nodes += 1
+          end
+          puts "Key: #{node.key}\n Value: #{node.value}\n\n"
+        end
+      end
+
+
+    end
+    puts "Total Nodes: #{total_nodes}\nArray Size: #{size}\nLoad Factor: #{print_load_factor}"
+  end
+
+  def print_load_factor
+    total_nodes = 0
+
+    (0...size).each do |index|
+      linked_list = @items[index]
+      if linked_list != nil
+        node = linked_list.head
+        if node.next == nil
+          total_nodes += 1
+        else
+          total_nodes += 1
+          until node.next == nil
+            node = node.next
+            total_nodes += 1
+          end
+        end
+      end
+    end
+
+    load_factor_calculation = total_nodes.to_f / size
   end
 end
