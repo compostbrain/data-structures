4df5dbbea875b2986f63b967b62d65e2195ba9c8
compostbrain <joelcahalan@gmail.com>
2017-12-14 10:05:25 -0500

    requested revisions implemented

--- 02-Algorithms-and-Complexity/03-Algorithms-Sorting/quick_sort.rb
+++ 02-Algorithms-and-Complexity/03-Algorithms-Sorting/quick_sort.rb
@@ -1,19 +1,35 @@
-def quick_sort(collection)
-  return collection if collection.length < 2
-  left, right = [], [] # partion lists
 
-  pivot = collection.length - 1 # default pivot is last item in collection
-  pivot_value = collection[pivot] # set the pivot value
-
-  collection = collection.slice(0, pivot)
-
-  for item in collection
-    item < pivot_value ? left.push(item) : right.push(item)
+def quick_sort(container)
+  bottom, top = [], []
+  top[0] = 0
+  bottom[0] = container.size
+  i = 0
+  while i >= 0 do
+    l = top[i]
+    r = bottom[i] - 1;
+    if l < r
+      pivot = container[l]
+      while l < r do
+        r -= 1 while (container[r] >= pivot  && l < r)
+        if (l < r)
+          container[l] = container[r]
+          l += 1
+        end
+        l += 1 while (container[l] <= pivot  && l < r)
+        if (l < r)
+          container[r] = container[l]
+          r -= 1
+        end
+      end
+      container[l] = pivot
+      top[i+1] = l + 1
+      bottom[i+1] = bottom[i]
+      bottom[i] = l
+      i += 1
+    else
+      i -= 1
+    end
   end
-
-  return quick_sort(left).concat([pivot_value], quick_sort(right))
-
-
+  container
 end
-
 p quick_sort([5,7,2,1,46,5,78,6])
