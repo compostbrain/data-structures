e6ff3fe098d840d9560ee4b5b8e75b170c487bc3
compostbrain <joelcahalan@gmail.com>
2017-12-18 11:44:23 -0500

    implemented interative in place quick sort

--- 02-Algorithms-and-Complexity/06-Improving-Complexity/improving_complexity_version_three.rb
+++ 02-Algorithms-and-Complexity/06-Improving-Complexity/improving_complexity_version_three.rb
@@ -5,18 +5,37 @@ def poorly_written_ruby(*arrays)
   collection = arrays.flatten # use built in ruby method
 
   return collection if collection.length < 2
-  left, right = [], [] # partion lists
-
-  pivot = collection.length - 1 # default pivot is last item in collection
-  pivot_value = collection[pivot] # set the pivot value
-
-  collection = collection.slice(0, pivot)
-
-  for item in collection
-    item < pivot_value ? left.push(item) : right.push(item)
-  end
-
-  return poorly_written_ruby(left).concat([pivot_value], poorly_written_ruby(right))
+  bottom, top = [], []
+ top[0] = 0
+ bottom[0] = collection.size
+ i = 0
+ while i >= 0 do
+   l = top[i]
+   r = bottom[i] - 1;
+   if l < r
+     pivot = collection[l]
+     while l < r do
+       r -= 1 while (collection[r] >= pivot  && l < r)
+       if (l < r)
+         collection[l] = collection[r]
+         l += 1
+       end
+       l += 1 while (collection[l] <= pivot  && l < r)
+       if (l < r)
+         collection[r] = collection[l]
+         r -= 1
+       end
+     end
+     collection[l] = pivot
+     top[i+1] = l + 1
+     bottom[i+1] = bottom[i]
+     bottom[i] = l
+     i += 1
+   else
+     i -= 1
+   end
+ end
+ collection
 end
 
 
